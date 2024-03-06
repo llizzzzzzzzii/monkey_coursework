79f1486cd6b6bc0ca39283a92c5df50fa8b696ac
oleole23 <125196739+oleole23@users.noreply.github.com>
2024-03-06 14:29:11 +0300

    MONKEY-20 Add Clicker

---
+++ specie/Clicker.py
@@ -0,0 +1,91 @@
+import time
+import random
+from logi.loger import LogClicker
+
+def find_locators(page):
+    clickable_elements = page.query_selector_all('button, a, input, [role="button"]')
+    visible_clickable_elements = [element for element in clickable_elements if element.is_visible()]
+    return visible_clickable_elements
+
+def draw_indicator(page, x, y):
+    script = (f'document.body.insertAdjacentHTML("beforeend", "<div style=\'position: absolute; left: {x}px; '
+              f'top: {y}px; width: 10px; height: 10px; background-color: red; border-radius: 50%;\'></div>");')
+    page.evaluate(script)
+    time.sleep(0.7)
+    script_remove = 'document.body.lastChild.remove();'
+    page.evaluate(script_remove)
+
+def random_action():
+    actions = [hover, click, double_click, multiple_click, click_and_hold]
+    get_random = random.choice(actions)
+    return get_random
+
+def get_element_and_coordinate(page):
+    visible_elements = find_locators(page)
+    element = random.choice(visible_elements)
+    x, y = element.bounding_box()["x"], element.bounding_box()["y"]
+    height, width = element.bounding_box()["height"], element.bounding_box()["width"]
+    x = int(x + width / 2)
+    y = int(y + height / 2)
+    return element, x, y
+
+def click(page, indication):
+    element, x, y = get_element_and_coordinate(page)
+    try:
+        if indication:
+            draw_indicator(page, x, y)
+        element.click()
+    except Exception:
+        LogClicker.logger.exception("Error: Click failed")
+        exit()
+    LogClicker.logger.info(f"Clicked at position {x, y}")
+
+def double_click(page, indication):
+    element, x, y = get_element_and_coordinate(page)
+    try:
+        if indication:
+            draw_indicator(page, x, y)
+            draw_indicator(page, x, y)
+        element.dblclick()
+    except Exception:
+        LogClicker.logger.exception("Error: Double click failed")
+        exit()
+    LogClicker.logger.info(f"Clicked at position {x, y} 2 times")
+
+def multiple_click(page, indication):
+    element, x, y = get_element_and_coordinate(page)
+    count = random.randint(3, 10)
+    try:
+        for i in range(count):
+            if indication:
+                draw_indicator(page, x, y)
+            element.click()
+    except Exception:
+        LogClicker.logger.exception("Error: Multiple clicks failed")
+        exit()
+    LogClicker.logger.info(f"Clicked at position {x, y} {count} times")
+
+def hover(page, indication):
+    element, x, y = get_element_and_coordinate(page)
+    try:
+        if indication:
+            draw_indicator(page, x, y)
+        element.hover()
+    except Exception:
+        LogClicker.logger.exception("Error: Hover failed")
+        exit()
+    LogClicker.logger.info(f"Hovered at position {x, y}")
+
+def click_and_hold(page, indication):
+    element, x, y = get_element_and_coordinate(page)
+    try:
+        if indication:
+            draw_indicator(page, x, y)
+        page.mouse.move(x, y)
+        page.mouse.down()
+        time.sleep(0.7)
+        page.mouse.up()
+    except Exception:
+        LogClicker.logger.exception("Error: Click and hold failed")
+        exit()
+    LogClicker.logger.info(f"Clicked and held at position {x, y}")
