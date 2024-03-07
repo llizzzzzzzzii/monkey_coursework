3a27bedc4097eef26ec44cb1c867329fb73aad51
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2024-03-07 10:17:15 +0300

    MONKEY-18 Fix structure

--- locators.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-def get_random_coordinate(w_size):
-    x = random.randint(0, w_size['width'])
-    y = random.randint(0, w_size['height'])
-    return x, y
--- specie/Typer.py
+++ specie/Typer.py
@@ -3,20 +3,23 @@ import time
 from logi.loger import LogTyper
 from logi.loger import LogError
 
+
 def blocking_movement(page, element):
     page.evaluate(
         '(element) => { element.addEventListener("keydown", (e) => { if (!element.classList.contains("no-enter") && '
         'e.key === "Enter") { e.preventDefault(); e.stopPropagation(); } }); }',
         element)
 
+
 def find_locators(page):
-    input_elements = page.query_selector_all('input, textarea, [contenteditable=true]')
+    input_elements = page.query_selector_all('input, textarea, div[contenteditable=true]')
     viewport_height = page.viewport_size['height']
     visible_input_elements = [element for element in input_elements if
                               element.is_visible() and element.bounding_box()['y'] >= 0 and
                               element.bounding_box()['y'] <= viewport_height and
                               element.get_attribute("type") not in ["radio", "checkbox", "submit", "button"]]
     return visible_input_elements
+
 
 def send_text(page, indication, restricted_page):
     page.wait_for_load_state("networkidle")
@@ -59,33 +62,35 @@ def send_keys(page, indication, restricted_page):
     input_type = ['Shift', 'Backspace', 'Control', 'Escape', 'Alt', 'Delete', 'Enter']
     random_input_type = random.choice(input_type)
     try:
-        if restricted_page:
-            if random_input_element.get_attribute("value") is not None and random_input_element.get_attribute(
-                    "value") != "":
-                random_input_element.fill("")
+        if random_input_element:
+            if restricted_page:
+                if random_input_element.get_attribute("value") is not None and random_input_element.get_attribute(
+                        "value") != "":
+                    random_input_element.fill("")
 
-            blocking_movement(page, random_input_element)
+                blocking_movement(page, random_input_element)
 
-        page.wait_for_load_state("networkidle")
-        if indication is True:
-            page.evaluate(
-                """(element) => {
-                       element.style.backgroundColor = "rgba(255,0,0,0.7)";
-                   }""",
-                random_input_element,
-            )
-            time.sleep(0.5)
-            page.evaluate(
-                """(element) => {
-                       element.style.backgroundColor = "transparent";
-                   }""",
-                random_input_element,
-            )
-        random_input_element.press(random_input_type)
+            page.wait_for_load_state("networkidle")
+            if indication is True:
+                page.evaluate(
+                    """(element) => {
+                           element.style.backgroundColor = "rgba(255,0,0,0.7)";
+                       }""",
+                    random_input_element,
+                )
+                time.sleep(0.5)
+                page.evaluate(
+                    """(element) => {
+                           element.style.backgroundColor = "transparent";
+                       }""",
+                    random_input_element,
+                )
+            random_input_element.press(random_input_type)
     except Exception:
         LogError.logger.exception("Sent key failed")
         exit()
     LogTyper.logger.info(f"Sent {random_input_type} key to a text element at position {x, y}")
+
 
 def get_random_action():
     rand_action = ['text', 'keys']
