441badcf2632f9ca6023fb8f00dd03f5c407ea44
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2024-04-02 16:37:49 +0300

    MONKEY - 30 Add tests

---
+++ tests/test_send_keys.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import sync_playwright
+import pytest
+from specie.typer import send_keys
+
+
+@pytest.fixture
+def browser_page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        yield page
+        browser.close()
+
+
+def test_send_keys(browser_page):
+    browser_page.goto("https://account.mail.ru/signup")
+    indication = True
+    restricted_page = False
+    send_keys(browser_page, indication, restricted_page)
+    input_value = browser_page.evaluate('''() => {
+            return document.querySelector('input').value;
+        }''')
+    assert input_value is not None
