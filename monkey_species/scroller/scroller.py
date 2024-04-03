1bc7fba1534f73bcadab2407c3442623047cc74a
oleole23 <125196739+oleole23@users.noreply.github.com>
2024-04-03 22:28:41 +0300

    MONKEY-2 Add ignore errors for scroller

--- monkey_species/scroller/scroller.py
+++ monkey_species/scroller/scroller.py
@@ -3,7 +3,7 @@ from monkey_logging.monkey_logger import LogError
 import random
 
 
-def scroll_to_random_position(page):
+def scroll_to_random_position(page, ignore_errors):
     try:
         width = page.evaluate('() => document.body.scrollWidth')
         height = page.evaluate('() => document.body.scrollHeight')
@@ -13,6 +13,10 @@ def scroll_to_random_position(page):
 
         page.evaluate('window.scrollTo({}, {})'.format(random_x, random_y))
         LogScroller.logger.info(f"Scrolled to position {random_x, random_y}")
-    except Exception:
-        LogError.logger.error("Scroll failed")
+    except Exception as e:
+        LogScroller.logger.error("Error: Scroll failed")
+        LogError.logger.error(f"{type(e).__name__}: {str(e)}", exc_info=True)
+        if not ignore_errors:
+            return False
+    return True
 
