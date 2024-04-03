e4bb314d93174836a67e2512ed1a96560bf99a0d
oleole23 <125196739+oleole23@users.noreply.github.com>
2024-04-03 23:16:23 +0300

    MONKEY-2 Add ignore errors for typer

--- monkey_species/typer/typer.py
+++ monkey_species/typer/typer.py
@@ -27,70 +27,74 @@ def find_locators(page):
     return visible_input_elements
 
 
-def send_text(page, indication, restricted_page):
+def send_text(page, indication, restricted_page, ignore_errors):
     page.wait_for_load_state("networkidle")
     random_text = get_random_string()
     random_input_element = random.choice(find_locators(page))
     x, y = int(random_input_element.bounding_box()["x"]), int(random_input_element.bounding_box()["y"])
     try:
-        if random_input_element:
-            if random_input_element.get_attribute("value") is not None and random_input_element.get_attribute(
-                    "value") != "":
-                random_input_element.fill("")
-            page.wait_for_load_state("networkidle")
-            if indication is True:
-                page.evaluate(
-                    """(element) => {
-                           element.style.backgroundColor = "rgba(255,0,0,0.7)";
-                       }""",
-                    random_input_element,
-                )
-                time.sleep(0.5)
-                page.evaluate(
-                    """(element) => {
-                           element.style.backgroundColor = "transparent";
-                       }""",
-                    random_input_element,
-                )
-            random_input_element.fill(random_text)
-    except Exception:
-        LogError.logger.exception("Typed text failed")
-        exit()
-    LogTyper.logger.info(f"Typed {random_text} into a text element at position {x, y}")
+        if random_input_element.get_attribute("value") is not None and random_input_element.get_attribute(
+                "value") != "":
+            random_input_element.fill("")
+        page.wait_for_load_state("networkidle")
+        if indication is True:
+            page.evaluate(
+                """(element) => {
+                       element.style.backgroundColor = "rgba(255,0,0,0.7)";
+                   }""",
+                random_input_element,
+            )
+            time.sleep(0.5)
+            page.evaluate(
+                """(element) => {
+                       element.style.backgroundColor = "transparent";
+                   }""",
+                random_input_element,
+            )
+        random_input_element.fill(random_text)
+        LogTyper.logger.info(f"Typed {random_text} into a text element at position {x, y}")
+    except Exception as e:
+        LogTyper.logger.error("Error: Typed text failed")
+        LogError.logger.error(f"{type(e).__name__}: {str(e)}", exc_info=True)
+        if not ignore_errors:
+            return False
+    return True
 
 
-def send_keys(page, indication, restricted_page):
+
+def send_keys(page, indication, restricted_page, ignore_errors):
     initial_url = page.url
     page.wait_for_load_state("networkidle")
     random_input_element = random.choice(find_locators(page))
     x, y = int(random_input_element.bounding_box()["x"]), int(random_input_element.bounding_box()["y"])
-    input_type = ['Enter']
-    # input_type = ['Shift', 'Backspace', 'Control', 'Escape', 'Alt', 'Delete', 'Enter']
+    input_type = ['Shift', 'Backspace', 'Control', 'Escape', 'Alt', 'Delete', 'Enter']
     random_input_type = random.choice(input_type)
     try:
-        if random_input_element:
-            page.wait_for_load_state("networkidle")
-            if indication is True:
-                page.evaluate(
-                    """(element) => {
-                           element.style.backgroundColor = "rgba(255,0,0,0.7)";
-                       }""",
-                    random_input_element,
-                )
-                time.sleep(0.5)
-                page.evaluate(
-                    """(element) => {
-                           element.style.backgroundColor = "transparent";
-                       }""",
-                    random_input_element,
-                )
-            random_input_element.press(random_input_type)
-            if restricted_page:
-                blocking_movement(page, initial_url)
-    except Exception:
-        LogError.logger.exception("Sent key failed")
-        exit()
-    LogTyper.logger.info(f"Sent {random_input_type} key to a text element at position {x, y}")
+        page.wait_for_load_state("networkidle")
+        if indication is True:
+            page.evaluate(
+                """(element) => {
+                       element.style.backgroundColor = "rgba(255,0,0,0.7)";
+                   }""",
+                random_input_element,
+            )
+            time.sleep(0.5)
+            page.evaluate(
+                """(element) => {
+                       element.style.backgroundColor = "transparent";
+                   }""",
+                random_input_element,
+            )
+        random_input_element.press(random_input_type)
+        if restricted_page:
+            blocking_movement(page, initial_url)
+        LogTyper.logger.info(f"Sent {random_input_type} key to a text element at position {x, y}")
+    except Exception as e:
+        LogTyper.logger.error("Error: Sent key failed")
+        LogError.logger.error(f"{type(e).__name__}: {str(e)}", exc_info=True)
+        if not ignore_errors:
+            return False
+    return True
 
 
 def get_random_action():
