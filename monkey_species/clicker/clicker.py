2cb8c1a62d7cc8f4646cafc9343a1f1c714b523f
oleole23 <125196739+oleole23@users.noreply.github.com>
2024-06-02 22:31:14 +0300

    MONKEY-24 Fix indication in clicker

--- monkey_species/clicker/clicker.py
+++ monkey_species/clicker/clicker.py
@@ -16,36 +16,40 @@ def find_locators(page):
     page.wait_for_load_state("load")
     clickable_elements = page.query_selector_all('button, a, input, img, input[role="button"]')
     viewport_height = page.viewport_size['height']
-    visible_clickable_elements = [element for element in clickable_elements if element.is_visible() and
-                                  0 <= element.bounding_box()['y'] <= viewport_height
-                                  and element.get_attribute('type') != 'url']
+    visible_clickable_elements = [element for element in clickable_elements if is_element_visible(page, element) and
+                                  0 <= element.bounding_box()['y'] <= viewport_height]
+    # visible_clickable_elements = [element for element in clickable_elements if element.is_visible() and
+    #                               0 <= element.bounding_box()['y'] <= viewport_height
+    #                               and element.get_attribute('type') != 'url']
     return visible_clickable_elements
 
 
-def draw_indicator(page, element, color):
+def draw_indicator(page, x, y, color):
     rgba_color = to_rgba(color, alpha=0.7)
     rgba_str = f"rgba({int(rgba_color[0] * 255)},{int(rgba_color[1] * 255)},{int(rgba_color[2] * 255)},{rgba_color[3]})"
-    box = element.bounding_box()
-    x = box['x'] + box['width'] / 2
-    y = box['y'] + box['height'] / 2
-    page.evaluate(f'''
-      circle = document.createElement('div');
-      circle.style.width = '15px';
-      circle.style.height = '15px';
-      circle.style.borderRadius = '50%';
-      circle.style.backgroundColor = '{rgba_str}';
-      circle.style.position = 'absolute';
-      circle.style.left = '{x}px';
-      circle.style.top = '{y}px';
-      circle.style.zIndex = '10000';
-      circle.style.pointerEvents = 'none';
-      document.body.appendChild(circle);
-      setTimeout(() => circle.remove(), 1000);
-    ''')
+    radius = 15
+    page.evaluate(f"""
+        const indicator = document.createElement('div');
+        indicator.style.cssText = `
+            position: fixed !important;
+            background-color: {rgba_str} !important;
+            width: {radius}px !important;
+            height: {radius}px !important;
+            border: 2px solid red !important;
+            border-radius: 50% !important;
+            left: {x}px !important;
+            top: {y}px !important;
+            transform: translate(-50%, -50%) !important;
+            z-index: 2147483647 !important;
+        `;
+        document.body.appendChild(indicator);
+        setTimeout(() => indicator.remove(), 1000);
+    """)
 
 
 def random_action():
-    actions = [hover, click, double_click, multiple_click, click_and_hold]
+    # actions = [hover, click, double_click, multiple_click, click_and_hold]
+    actions = [click]
     get_random = random.choice(actions)
     return get_random
 
@@ -57,20 +61,45 @@ def get_element_and_coordinate(page):
         LogClicker.logger.warning("Warning: The element was not found")
         return [], 0, 0
     element = random.choice(visible_elements)
-    x, y = int(element.bounding_box()["x"]), int(element.bounding_box()["y"])
+    bounding_box = element.bounding_box()
+    x, y = int(bounding_box['x'] + bounding_box['width'] / 2), int(bounding_box['y'] + bounding_box['height'] / 2)
     return element, x, y
+
+
+def is_element_visible(page, element):
+    return page.evaluate("""
+        (element) => {
+            const style = window.getComputedStyle(element);
+            if (style.display === 'none' || style.visibility === 'hidden' || style.opacity === '0' ||
+                element.offsetWidth === 0 || element.offsetHeight === 0) {
+                return false;
+            }
+
+            const rect = element.getBoundingClientRect();
+            const centerX = rect.left + rect.width / 2;
+            const centerY = rect.top + rect.height / 2;
+            const topElement = document.elementFromPoint(centerX, centerY);
+
+            return element === topElement || element.contains(topElement);
+        }
+    """, element)
 
 
 def click(page, indication, restricted_page, color):
     element, x, y = get_element_and_coordinate(page)
     tag_name = page.evaluate("(element) => element.tagName.toLowerCase()", element)
+    has_href = page.evaluate("(element) => element.hasAttribute('href')", element)
     if not element:
         return
     initial_url = page.url
     try:
         if indication:
-            draw_indicator(page, element, color)
-        with page.expect_navigation():
+            draw_indicator(page, x, y, color)
+            time.sleep(1)
+        if has_href:
+            with page.expect_navigation():
+                page.mouse.click(x, y)
+        else:
             page.mouse.click(x, y)
         if tag_name == 'img':
             time.sleep(0.1)
