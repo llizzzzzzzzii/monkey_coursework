54f87aee27d7a9671716c72747f9be9f8ff3fb3a
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2024-06-06 18:19:30 +0300

    MONKEY - 30 Add test to check for non-clickable images

--- tests/is_image_and_has_target_and_href/test_is_image_and_has_target_and_href.py
+++ tests/is_image_and_has_target_and_href/test_is_image_and_has_target_and_href.py
@@ -4,6 +4,7 @@ from monkey_logging.monkey_logger import LogMonkey
 from monkey_logging.monkey_logger import LogError
 from monkey_species.clicker.clicker import is_image_and_has_target_blank_and_href
 
+
 @pytest.fixture
 def browser_page():
     try:
@@ -16,10 +17,21 @@ def browser_page():
         LogMonkey.logger.error("Error: Loading browser")
         LogError.logger.error(f"{type(e).__name__}: {str(e)}", exc_info=True)
 
+
 def test_image_has_href(browser_page):
     page = browser_page
     page.goto('https://alfabank.ru/financial/')
     index = 0
     element = page.locator(
-        'img[alt="иконка карточки"].a2lPS.e2lPS[loading="lazy"][height="40"][src="https://alfabank.servicecdn.ru/media/main-page-assets/main-page_selfemployed-icon.svg"][width="40"]').nth(index).element_handle()
+        'img[alt="иконка карточки"].a2lPS.e2lPS[loading="lazy"][height="40"][src="https://alfabank.servicecdn.ru/media/main-page-assets/main-page_selfemployed-icon.svg"][width="40"]').nth(
+        index).element_handle()
     assert is_image_and_has_target_blank_and_href(page, element) == (False, True, 'img')
+
+
+def test_image_has_not_href(browser_page):
+    page = browser_page
+    page.goto('https://cashpo-design.ru/news/kak-ukhazhivat-za-orkhideei-v-domashnikh-usloviyakh')
+    element = page.locator(
+        'img[src="userfiles/новости/orkhidei-2.jpg"][alt="Орхидеи в горшках в интерьере"].js-qazy-loaded').element_handle()
+    print(element)
+    assert is_image_and_has_target_blank_and_href(page, element) == (False, False, 'img')
