7819b9b2aec88cb5ab3402be4ab64b9cad8526a6
oleole23 <125196739+oleole23@users.noreply.github.com>
2024-06-06 20:01:37 +0300

    MONKEY-31 Updated structure of toucher

--- monkey_species/toucher/toucher.py
+++ monkey_species/toucher/toucher.py
@@ -2,38 +2,11 @@ from monkey_logging.monkey_logger import LogToucher
 from monkey_logging.monkey_logger import LogError
 from playwright._impl._errors import TimeoutError as PlaywrightTimeoutError
 from indicattion.clicker_toucher_indication import draw_indicator
-from locators.clicker_toucher_locators import find_locators
 from locators.clicker_toucher_locators import is_image_and_has_target_blank_and_href
-import random
+from monkey_species.toucher.toucher_handler import get_element_and_coordinate
+from monkey_species.toucher.toucher_handler import open_new_tab
+from monkey_species.toucher.toucher_handler import blocking_movement
 import time
-
-
-def get_element_and_coordinate(page):
-    page.wait_for_load_state("domcontentloaded")
-    visible_elements = find_locators(page)
-    if not visible_elements:
-        LogToucher.logger.warning("Warning: The element was not found")
-        return [], -1, -1
-    element = random.choice(visible_elements)
-    bounding_box = element.bounding_box()
-    x, y = int(bounding_box['x'] + bounding_box['width'] / 2), int(bounding_box['y'] + bounding_box['height'] / 2)
-    return element, x, y
-
-
-def blocking_movement(page, initial_url):
-    current_url = page.url
-    if current_url != initial_url:
-        page.goto(initial_url)
-
-
-def open_new_tab(page, x, y, restricted_page):
-    with page.context.expect_page() as new_page_info:
-        page.touchscreen.tap(x, y)
-    if not restricted_page:
-        new_page = new_page_info.value
-        new_page.bring_to_front()
-        return new_page
-    return page
 
 
 def touch(page, indication, restricted_page, color):
