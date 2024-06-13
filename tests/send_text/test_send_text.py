9c13f799c7a34f45fb42ef5f3bce0fa842cac470
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2024-06-13 17:22:29 +0300

    MONKEY - 30 Update test for typer

--- tests/send_text/test_send_text.py
+++ tests/send_text/test_send_text.py
@@ -19,11 +19,12 @@ def browser_page():
 
 
 def test_send_text(browser_page):
-    browser_page.goto("https://account.mail.ru/signup")
+    browser_page.goto(
+        "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
     indication = True
     restricted_page = False
     color = 'blue'
-    selector = "input[id='x989c9bb8-39e5-41d9-adaf-d69add83a771'][class='input-0-2-119']"
+    selector = "input[id='searchInput'][title='Искать в Википедии [alt-shift-f]'][class='vector-search-box-input']"
     element = [selector]
     send_text(browser_page, indication, restricted_page, color, element)
     input_value = browser_page.evaluate('''() => {
