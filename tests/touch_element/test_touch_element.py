e2fdd02975c08536bc80bd057cb512f26b9087b9
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2024-06-13 16:45:03 +0300

    MONKEY - 30 Add test for toucher

---
+++ tests/touch_element/test_touch_element.py
@@ -0,0 +1,41 @@
+import pytest
+from playwright.sync_api import sync_playwright
+from monkey_species.toucher.toucher import touch
+from monkey_logging.monkey_logger import LogMonkey
+from monkey_logging.monkey_logger import LogError
+
+
+@pytest.fixture
+def browser_page():
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            yield browser
+            browser.close()
+    except Exception as e:
+        LogMonkey.logger.error("Error: Loading browser")
+        LogError.logger.error(f"{type(e).__name__}: {str(e)}", exc_info=True)
+
+
+def test_touch_without_change(browser_page):
+    context = browser_page.new_context(has_touch=True)
+    page = context.new_page()
+    url1 = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
+    page.goto(
+        "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+    selector = 'a[href="/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"][title="Перейти на заглавную страницу [alt-shift-z]"][accesskey="z"]'
+    element = [selector]
+    touch(page, True, False, 'red', element)
+    assert url1 == page.url
+
+
+def test_touch_with_change(browser_page):
+    context = browser_page.new_context(has_touch=True)
+    page = context.new_page()
+    url1 = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
+    page.goto(
+        "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+    selector = 'a[href="/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F:%D0%A1%D0%BE%D0%B4%D0%B5%D1%80%D0%B6%D0%B0%D0%BD%D0%B8%D0%B5"]'
+    element = [selector]
+    touch(page, True, False, 'red', element)
+    assert url1 != page.url
